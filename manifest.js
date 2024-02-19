8460b40ac2309ca4e259ffcce1832910790c602b
Mohammad Babaei <mohammadbabaeeweb.ir@gmail.com>
2024-02-19 04:25:56 +0330

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,23 @@
+{
+    "name": "To-Do-List",
+    "short_name": "ToDo",
+    "start_url": "index.html",
+    "display": "standalone",
+    "background_color": "#fe585a",
+    "theme_color": "#fe585a",
+    "orientation": "portrait-primary",
+    "icons": [
+        {
+            "src": "https://github.com/Mohammad-Babaei-DV/To-Do-List/new/main/a.png",
+            "type": "image/png",
+            "sizes": "512×512",
+            "purpose": "any maskable"
+        },
+        {
+            "src": "https://github.com/Mohammad-Babaei-DV/To-Do-List/new/main/b.png",
+            "sizes": "144x144",
+            "type": "image/png",
+            "purpose": "any"
+        }
+    ]
+}
